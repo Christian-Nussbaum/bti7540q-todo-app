67f525410be07ae81e5e006775fe562bb3f2dcae
Luca Berger <kucki10@hotmail.com>
2020-11-09 20:45:12 +0100

    Adjusted todo model, according to definition on Swagger API doc

--- src/app/model/todo.ts
+++ src/app/model/todo.ts
@@ -1,10 +1,16 @@
 export class Todo {
   id: number;
-  title = '';
-  isCompleted = false;
+  title: string;
+  category: string;
+  dueDate: string;
+  important: boolean;
+  completed = false;
 
-  public constructor(id: number, title: string) {
+  public constructor(id: number, title: string, category: string, dueDate: string, important: boolean) {
     this.id = id;
     this.title = title;
+    this.category = category;
+    this.dueDate = dueDate;
+    this.important = important;
   }
 }
